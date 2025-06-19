7f13c2699031d8ac72bb89b31b6e33637ee05c24
Phillip Gore <phillipagore@me.com>
2025-06-19 08:47:58 -0500

    image load fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,11 @@
 <script>
     import {base} from '$app/paths';
-	import pgAvatar from '$lib/images/pg-avatar.jpg';
 </script>
 
 <div class="bg-gray-darkest bg-pr-white">
     <div class="container-lg full-height bg-gray-lightest bg-pr-white box-shadow-10">
         <div class="pos-middle full-width txt-tn-center">
-            <img class="avatar img-height-tn-90 img-height-md-144" alt="Phillip's Face" src="{pgAvatar}" />
+            <img class="avatar img-height-tn-90 img-height-md-144" alt="Phillip's Face" src="{base}/src/lib/images/pg-avatar.jpg" />
             <h1 class="cl-danger txt-tn-center ln-height-125 p-tn-t-9 m-tn-b-27">
                 Welcome. 
                 <div class="subtitle cl-gray-dark cl-pr-black txt-tn-center m-tn-0">Which resume would you<br class="dis-sm-none"> like to view?</div>
