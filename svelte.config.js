ad5d71785b88e99c1daea94e17974609147af4bd
Phillip Gore <phillipagore@me.com>
2025-06-19 13:49:44 -0500

    image reset

--- svelte.config.js
+++ svelte.config.js
@@ -3,11 +3,12 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const dev = process.argv.includes('dev');
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: [vitePreprocess()],
 	kit: {
 		adapter: adapter(),
+		paths: {
+			base: dev ? '' : '/phillip_gore_sv'
+		}
 	},
 };
 
