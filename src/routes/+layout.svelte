302cb871318d7fa6de7fc1d5a9434523385564ba
Phillip Gore <phillipagore@me.com>
2025-06-19 08:54:09 -0500

    image load fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
   import '$lib/stylesheets/styles.css';
+	import pgAvatar from '$lib/images/pg-avatar.jpg';
 </script>
+
+<svelte:head>
+	<title>Phillip Gore</title>
+  <link rel="preload" href="{pgAvatar}" />
+</svelte:head>
 
 <slot />
