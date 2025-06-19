20f31699f7fd6053a11ef4b329874d7bccf9c68d
Phillip Gore <phillipagore@me.com>
2025-06-19 09:55:26 -0500

    image load fix

--- src/lib/components/DevTitle.svelte
+++ src/lib/components/DevTitle.svelte
@@ -1,5 +1,5 @@
 <section class="clearfix txt-md-center">
-    <enhanced:img class="avatar img-height-tn-90 img-height-md-144" src="$lib/assets/pg-avatar.jpg" alt="hillip's Face" />
+    <enhanced:img class="avatar img-height-tn-90 img-height-md-144" src="$lib/assets/pg-avatar.jpg" alt="Phillip's Face" />
     <div class="dis-xs-inline-block">
         <h1 class="cl-danger txt-tn-center ln-height-125 p-tn-t-9 m-tn-0">
             Phillip Gore
