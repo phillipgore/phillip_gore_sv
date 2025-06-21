95f95bcf966c7beb5b3a58abf81a867ef89e60f4
Phillip Gore <phillipagore@me.com>
2025-06-20 20:11:44 -0500

    gifts

---
+++ src/lib/components/PreachGifts.svelte
@@ -0,0 +1,34 @@
+<section class="m-tn-b-neg-12">
+    <h2 class="cl-danger m-tn-b-12 pg-br-after-avoid">Gifts</h2>
+    <div class="row pg-br-inside-avoid">
+        <div class="col-sm-1of2 p-sm-r-9">
+            <h3>Speaking</h3>
+            <div class="progress-bar m-tn-b-12">
+                <div class="progress-meter progress-80"></div>
+            </div>
+            <h3>Teaching</h3>
+            <div class="progress-bar m-tn-b-12">
+                <div class="progress-meter progress-70"></div>
+            </div>
+            <h3>Serving</h3>
+            <div class="progress-bar m-tn-b-12">
+                <div class="progress-meter progress-25"></div>
+            </div>
+        </div>
+        <div class="col-sm-1of2 p-sm-l-9">
+            <h3>Administration</h3>
+            <div class="progress-bar m-tn-b-12">
+                <div class="progress-meter progress-65"></div>
+            </div>
+            <h3>Leading</h3>
+            <div class="progress-bar m-tn-b-12">
+                <div class="progress-meter progress-60"></div>
+            </div>
+            <h3>Helps</h3>
+            <div class="progress-bar m-tn-b-12">
+                <div class="progress-meter progress-25"></div>
+            </div>
+        </div>
+    </div>
+
+</section>
