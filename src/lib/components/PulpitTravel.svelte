c7a94be03d5e52ee51e176165e2ce480c5d09f6e
Phillip Gore <phillipagore@me.com>
2026-02-06 21:12:34 -0600

    map refinement

--- src/lib/components/PulpitTravel.svelte
+++ src/lib/components/PulpitTravel.svelte
@@ -6,7 +6,15 @@
     <h2 class="cl-danger m-tn-b-12 pg-br-after-avoid">Travel</h2>
     <p class="pg-br-inside-avoid m-tn-b-18">I will generally travel up to 2 hours from my home in <span class="text-rtl txt-tn-left">OM ailadnaV</span>. We may be able to work something out for distances greater than 2 hours depending on the situation.</p>
     <div style="width: 100%; height: 21.875rem;">
-        <Map options={{ center: [39.311, -91.490], zoom: 7 }}>
+        <Map options={{ 
+            center: [39.311, -91.490], 
+            zoom: 7, 
+            zoomControl: false,
+            dragging: false,
+            scrollWheelZoom: false,
+            doubleClickZoom: false,
+			attributionControl: false
+        }}>
             <TileLayer url={'https://tile.openstreetmap.org/{z}/{x}/{y}.png'} />
             <Circle 
                 latLng={[39.311, -91.490]}
