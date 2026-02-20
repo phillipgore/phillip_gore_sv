fa43efdc72fd3d37408245b3005443f0ee97e398
Phillip Gore <phillipagore@me.com>
2026-02-19 18:34:02 -0600

    adjustment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,9 +11,13 @@
                 Welcome. 
                 <div class="subtitle cl-gray-dark cl-pr-black txt-tn-center m-tn-0">Which would you<br class="dis-sm-none"> like to view?</div>
             </h1>
-            <a href="{base}/developer" class="bg-gray-lighter cl-gray-darker bd-radius-3 p-tn-tb-6 p-tn-rl-18 m-tn-r-3">Developer Resume</a> 
-            <a href="{base}/preacher" class="bg-gray-lighter cl-gray-darker bd-radius-3 p-tn-tb-6 p-tn-rl-18 m-tn-l-3">Preacher Resume</a>
-            <a href="{base}/pulpit-supply" class="bg-gray-lighter cl-gray-darker bd-radius-3 p-tn-tb-6 p-tn-rl-18 m-tn-l-3">Pulpit Supply</a>
+            <div class="p-tn-b-1">
+                <a href="{base}/developer" class="bg-gray-lighter cl-gray-darker bd-radius-3 p-tn-tb-6 p-tn-rl-18 m-tn-r-3">Developer Resume</a> 
+                <a href="{base}/preacher" class="bg-gray-lighter cl-gray-darker bd-radius-3 p-tn-tb-6 p-tn-rl-18 m-tn-l-3">Preacher Resume</a>
+            </div>
+            <div class="p-tn-t-21">
+                <a href="{base}/pulpit-supply" class="bg-gray-lighter cl-gray-darker bd-radius-3 p-tn-tb-6 p-tn-rl-18 m-tn-l-3">Pulpit Supply</a>
+            </div>
         </div>
     </div>
 </div>
